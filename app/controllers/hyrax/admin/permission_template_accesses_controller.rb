bb9e79ee0ee6ef17c3ce5e7bd052d1b59f72145b
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-08-09 17:04:20 -0700

    Hyrax::Workflow::NotificationService - revise hash keys as String for #recipients
    Stop caring about hash keys in workflow notifications
    
    Revert committed WIP work and add debugging
    
    Admin Set Managers should be granted all registered roles on an active workflow, not just approving
    
    Also, fix bug with `#grant_workflow_roles` that prevents creating a new corresponding workflow role when switching workflows
    
    Checkpoint commit to create a WIP PR. Have four tests failing, and am struggling to remember what state I was in before moving off this branch.
    
    Rollback some of the changes made to the PTForm to get tests passing again.
    
    Up next: more testing in the UI to make sure AS participants get the right roles in the right workflows at the right times.
    
    add some integration tests
    
    needs more in-browser testing to make sure the right wf roles are being created

--- app/controllers/hyrax/admin/permission_template_accesses_controller.rb
+++ app/controllers/hyrax/admin/permission_template_accesses_controller.rb
@@ -29,6 +29,7 @@ module Hyrax
         end
 
         def update_management
+          # Forms::PermissionTemplateForm.new(@permission_template_access.permission_template).remove_manager!
           Forms::PermissionTemplateForm.new(@permission_template_access.permission_template).update_management
         end
     end

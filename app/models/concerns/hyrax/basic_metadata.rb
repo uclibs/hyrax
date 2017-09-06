531959825157875800a72d48511a23fb6001db83
Tom Johnson <tom@dp.la>
2017-09-06 10:34:27 -0700

    Testing Schema for Hyrax::BasicMetadata

--- app/models/concerns/hyrax/basic_metadata.rb
+++ app/models/concerns/hyrax/basic_metadata.rb
@@ -1,4 +1,31 @@
 module Hyrax
+  class BasicSchema < ActiveTriples::Schema
+    property :label, predicate: ActiveFedora::RDF::Fcrepo::Model.downloadFilename, multiple: false
+
+    property :relative_path, predicate: ::RDF::URI.new('http://scholarsphere.psu.edu/ns#relativePath'), multiple: false
+
+    property :import_url, predicate: ::RDF::URI.new('http://scholarsphere.psu.edu/ns#importUrl'), multiple: false
+    property :resource_type, predicate: ::RDF::Vocab::DC.type
+    property :creator, predicate: ::RDF::Vocab::DC11.creator
+    property :contributor, predicate: ::RDF::Vocab::DC11.contributor
+    property :description, predicate: ::RDF::Vocab::DC11.description
+    property :keyword, predicate: ::RDF::Vocab::DC11.relation
+    # Used for a license
+    property :license, predicate: ::RDF::Vocab::DC.rights
+
+    # This is for the rights statement
+    property :rights_statement, predicate: ::RDF::Vocab::EDM.rights
+    property :publisher, predicate: ::RDF::Vocab::DC11.publisher
+    property :date_created, predicate: ::RDF::Vocab::DC.created
+    property :subject, predicate: ::RDF::Vocab::DC11.subject
+    property :language, predicate: ::RDF::Vocab::DC11.language
+    property :identifier, predicate: ::RDF::Vocab::DC.identifier
+    property :based_near, predicate: ::RDF::Vocab::FOAF.based_near, class_name: ControlledVocabularies::Location
+    property :related_url, predicate: ::RDF::RDFS.seeAlso
+    property :bibliographic_citation, predicate: ::RDF::Vocab::DC.bibliographicCitation
+    property :source, predicate: ::RDF::Vocab::DC.source
+  end
+
   # An optional model mixin to define some simple properties. This must be mixed
   # after all other properties are defined because no other properties will
   # be defined once  accepts_nested_attributes_for is called
@@ -6,31 +33,7 @@ module Hyrax
     extend ActiveSupport::Concern
 
     included do
-      property :label, predicate: ActiveFedora::RDF::Fcrepo::Model.downloadFilename, multiple: false
-
-      property :relative_path, predicate: ::RDF::URI.new('http://scholarsphere.psu.edu/ns#relativePath'), multiple: false
-
-      property :import_url, predicate: ::RDF::URI.new('http://scholarsphere.psu.edu/ns#importUrl'), multiple: false
-      property :resource_type, predicate: ::RDF::Vocab::DC.type
-      property :creator, predicate: ::RDF::Vocab::DC11.creator
-      property :contributor, predicate: ::RDF::Vocab::DC11.contributor
-      property :description, predicate: ::RDF::Vocab::DC11.description
-      property :keyword, predicate: ::RDF::Vocab::DC11.relation
-      # Used for a license
-      property :license, predicate: ::RDF::Vocab::DC.rights
-
-      # This is for the rights statement
-      property :rights_statement, predicate: ::RDF::Vocab::EDM.rights
-      property :publisher, predicate: ::RDF::Vocab::DC11.publisher
-      property :date_created, predicate: ::RDF::Vocab::DC.created
-      property :subject, predicate: ::RDF::Vocab::DC11.subject
-      property :language, predicate: ::RDF::Vocab::DC11.language
-      property :identifier, predicate: ::RDF::Vocab::DC.identifier
-      property :based_near, predicate: ::RDF::Vocab::FOAF.based_near, class_name: ControlledVocabularies::Location
-      property :related_url, predicate: ::RDF::RDFS.seeAlso
-      property :bibliographic_citation, predicate: ::RDF::Vocab::DC.bibliographicCitation
-      property :source, predicate: ::RDF::Vocab::DC.source
-
+      apply_schema BasicSchema
       id_blank = proc { |attributes| attributes[:id].blank? }
 
       class_attribute :controlled_properties
